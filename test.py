88891a8d5a57534a6068c52cb91d442e15fd08e3
NicolasKnell <nicolasjv.knell@gmail.com>
2021-03-31 16:19:02 +0200

    Example of Bollinger Bands for the Microsoft Stock

--- test.py
+++ test.py
@@ -9,14 +9,31 @@ print('Hello World')
 
 
 msft = yf.Ticker("MSFT")
-df_msft = msft.history(period="1y")
+df_msft = msft.history(period="6mo")
+df_msft.to_csv('MSFT.csv')
 print(df_msft)
-rm_msft = df_msft['Open'].rolling(20).mean()
-rstd_msft = df_msft['Open'].rolling(20).std()
+rm_msft = df_msft['Open'].rolling(5).mean()
+rstd_msft = df_msft['Open'].rolling(5).std()
+daily_returns = (df_msft['Open']/df_msft['Open'].shift(1)) - 1
+print(daily_returns)
 
-df_msft['Open'].plot()
-rm_msft.plot(label = 'mean')
-(rm_msft-2*rstd_msft).plot(label = 'mean-2std')
-(rm_msft+2*rstd_msft).plot(label = 'mean+2std')
-plt.legend()
+fig, ax = plt.subplots(2, constrained_layout=True)
+
+ax[0].plot(df_msft['Open'], label = 'Stock Price')
+ax[0].plot(rm_msft, label = 'mean')
+ax[0].plot((rm_msft-2*rstd_msft), label = 'mean-2std')
+ax[0].plot((rm_msft+2*rstd_msft),label = 'mean+2std')
+ax[0].grid(True)
+ax[0].legend(loc = 'lower right')
+ax[0].tick_params('x', labelrotation=30)
+ax[0].set_title('Stock Price Variation')
+
+ax[1].plot(daily_returns, label = 'daily_returns')
+ax[1].tick_params('x', labelrotation=30)
+ax[1].legend(loc = 'lower right')
+ax[1].grid(True)
+ax[1].set_title('Stock Daily Return Variation')
+
 plt.show()
+
+
